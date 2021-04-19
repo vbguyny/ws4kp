05dbb03a2c999e6faa9ca8fc5c495fb9eed81e83
vbguyny <vb4guy@hotmail.com>
2021-04-19 19:24:21 -0400

    v1.59 - Changed position of navigation buttons to overlay on top of the WeatherStar 4000+ content when in fullscreen on a 4:3 display.

--- Scripts/index.js
+++ Scripts/index.js
@@ -142,6 +142,8 @@ var FullScreenResize = function ()
             {
                 LeftWidth = 60;
             }
+            divTwcLeft.find("div>div>a>img").css("width", "");
+            divTwcLeft.find(">div").css("padding-right", "12px").css("padding-left", "");
             divTwcLeft.attr("style", "width:" + LeftWidth + "px; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;");
             divTwcLeft.css("visibility", "visible");
             divTwcLeft.css("position", "");
@@ -152,6 +154,8 @@ var FullScreenResize = function ()
             {
                 RightWidth = 60;
             }
+            divTwcRight.find("div>div>a>img").css("width", "");
+            divTwcRight.find(">div").css("padding-left", "12px").css("padding-right", "");
             divTwcRight.attr("style", "width:" + RightWidth + "px; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;");
             divTwcRight.css("visibility", "visible");
             divTwcRight.css("position", "");
@@ -206,22 +210,26 @@ var FullScreenResize = function ()
             divTwcLeft.show();
             divTwcRight.show();
 
-            LeftWidth = ((WindowWidth - (WindowHeight * 1.33333333333333333333)) / 2);
-            if (LeftWidth < 60)
+            LeftWidth = (WindowWidth / 13);
+            if (LeftWidth < 25)
             {
-                LeftWidth = 60;
+                LeftWidth = 25;
             }
-            divTwcLeft.attr("style", "width:" + LeftWidth + "px; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:1000000;");
+            divTwcLeft.find("div>div>a>img").css("width", "100%");
+            divTwcLeft.find(">div").css("padding-right", "0px").css("padding-left", "12px");
+            divTwcLeft.attr("style", "width:" + LeftWidth + "px; height:" + IFrameHeight + "px; border:none; margin:0; padding:0; overflow:hidden; z-index:1000000;");
             divTwcLeft.css("visibility", "visible");
             divTwcLeft.css("position", "absolute");
             divTwcLeft.css("left", "0px");
 
-            RightWidth = ((WindowWidth - (WindowHeight * 1.33333333333333333333)) / 2);
-            if (RightWidth < 60)
+            RightWidth = (WindowWidth / 13);
+            if (RightWidth < 25)
             {
-                RightWidth = 60;
+                RightWidth = 25;
             }
-            divTwcRight.attr("style", "width:" + RightWidth + "px; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:1000000;");
+            divTwcRight.find("div>div>a>img").css("width", "100%");
+            divTwcRight.find(">div").css("padding-left", "0px").css("padding-right", "12px");
+            divTwcRight.attr("style", "width:" + RightWidth + "px; height:" + IFrameHeight + "px; border:none; margin:0; padding:0; overflow:hidden; z-index:1000000;");
             divTwcRight.css("visibility", "visible");
             divTwcRight.css("position", "absolute");
             divTwcRight.css("right", "0px");
@@ -313,7 +321,7 @@ var OnFullScreen = function ()
 
 var InFullScreen = function()
 {
-    //return true;
+    return true;
     //return (document.fullscreenEnabled || document.mozFullScreenEnabled || document.webkitFullscreenEnabled);
     //return (window.innerHeight == screen.height);
     //return (((document.fullScreenElement || document.mozFullScreenElement || document.webkitFullscreenElement) != null) || (window.innerHeight >= screen.height));
